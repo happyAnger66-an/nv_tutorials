592920c5482666c757051dd7a87873d7d0a14cfc
zhangxiaoan <happyAnger66@163.com>
2025-11-13 11:07:50 +0800

    feat: add xiaoan model example.

--- tensort-llm-examples/add_new_models/configuration_mymodel.py
+++
@@ -1,6 +0,0 @@
-from transformers.configuration_utils import PretrainedConfig
-from transformers.models.
-
-class MyModelConfig(PretrainedConfig):
-    def __init__(self):
-        super.__init__(self)
